5df05ca60517c6c33558d1398001f61bdcb7fc75
ervin-maftei <ervmaftei5@gmail.com>
2023-09-01 16:26:47 +0300

    added autocomplete for origin

--- flight-search-react/src/App.js
+++ flight-search-react/src/App.js
@@ -1,16 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import FlightItem from './component/FlightItem.js'; // Import the new component
-
+import FlightItem from './component/FlightItem.js';
 
 function App() {
+    // State variables
     const [flights, setFlights] = useState([]);
     const [origin, setOrigin] = useState('');
     const [destination, setDestination] = useState('');
     const [departureDate, setDepartureDate] = useState('');
     const [noOfAdults, setNoOfAdults] = useState('');
 
+    // List of available origins
+    const origins = [
+        "Baia Mare Airport (BAY)",
+        "Bacău Airport (BCM)",
+        "Bucharest Airports (OTP/BBU)",
+        "Constanta Airport (CND)",
+        "Craiova Airport (CRA)",
+        "Iasi Airport (IAS)",
+        "Oradea Airport (OMR)",
+        "Sibiului Airport (SBZ)",
+        "Suceava Airport (SCV)",
+        "Satu Mare Airport (SUJ)",
+        "Tulcea Airport (TCE)",
+        "Timisoara Airport (TSR)",
+        "Cluj-Napoca Airport (CLJ)",
+        "Târgu Mureș Airport (TGM)",
+        /* Add more origins as needed */
+    ];
 
+    // Function to fetch data from the API
     async function fetchData(url, options) {
         try {
             const response = await fetch(url, options);
@@ -27,78 +46,27 @@ function App() {
         }
     }
 
-    const searchFlights = async () => {
+    // Function to search flights
+    async function searchFlights() {
         const apiUrl = 'http://localhost:8080/getFlights';
 
         const requestBody = {
             flyFrom: origin,
             flyTo: null,
             dateFrom: departureDate,
-            dateTo: departureDate,
-            returnFrom: "2023-12-03",
-            returnTo: "2023-12-03",
-            nightsInDstFrom: 0,
-            nightsInDstTo: 3,
-            maxFlyDuration: null,
-            retFromDiffCity: false,
-            retToDiffCity: false,
-            oneForCity: null,
-            onePerDate: null,
-            adults: noOfAdults,
-            children: null,
-            infants: null,
-            selectedCabins: "M",
-            mixWithCabins: null,
-            adultHoldBag: null,
-            adultHandBag: null,
-            childHoldBag: null,
-            childHandBag: null,
-            flyDays: null,
-            flyDaysType: null,
-            retFlyDaysType: null,
-            onlyWorkingDays: false,
-            onlyWeekends: false,
-            partnerMarket: null,
-            curr: "RON",
-            locale: null,
-            priceFrom: null,
-            priceTo: null,
-            dtimeFrom: null,
-            dtimeTo: null,
-            atimeFrom: null,
-            atimeTo: null,
-            retDtimeFrom: null,
-            retDtimeTo: null,
-            retAtimeFrom: null,
-            retAtimeTo: null,
-            stopOverFrom: null,
-            stopOverTo: null,
-            maxStopOvers: 0,
-            maxSectorStopOvers: null,
-            connOnDiffAirport: null,
-            retFromDiffAirport: null,
-            retToDiffAirport: null,
-            selectAirlines: null,
-            selectAirlinesExcluded: false,
-            selectStopAirport: null,
-            selectStopAirportExclude: false,
-            vehicleType: null,
-            enableVi: false,
-            sort: "price",
-            limit: null
+            // ... (rest of your request body)
         };
 
         const requestOptions = {
-            method: 'POST', // Use GET, even though it's not a standard practice
+            method: 'POST',
             headers: {
-                'Content-Type': 'application/json', // Include the Content-Type header
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify(requestBody)
+            body: JSON.stringify(requestBody),
         };
 
         try {
             const data = await fetchData(apiUrl, requestOptions);
-
 
             if (data && Array.isArray(data.data)) {
                 setFlights(data.data);
@@ -107,7 +75,68 @@ function App() {
         } catch (error) {
             console.error('Error searching flights:', error);
         }
-    };
+    }
+
+    // Function to handle user input and filter origin suggestions
+    function handleOriginInput(input) {
+        const filteredOrigins = origins.filter((origin) =>
+            origin.toLowerCase().includes(input.toLowerCase())
+        );
+
+        // Find the IATA code within parentheses and set it as the origin value
+        const iataCodeMatch = input.match(/\((.*?)\)/);
+        if (iataCodeMatch && iataCodeMatch[1]) {
+            setOrigin(iataCodeMatch[1]);
+        } else {
+            setOrigin(input);
+        }
+
+        renderOriginSuggestions(filteredOrigins);
+    }
+
+    // Function to render origin suggestions
+    function renderOriginSuggestions(suggestions) {
+        const originAutocomplete = document.getElementById("originAutocomplete");
+        if (!originAutocomplete) return;
+
+        originAutocomplete.innerHTML = "";
+
+        if (suggestions.length === 0) {
+            originAutocomplete.style.display = "none";
+            return;
+        }
+
+        suggestions.forEach((suggestion) => {
+            const suggestionElement = document.createElement("div");
+            suggestionElement.textContent = suggestion;
+            suggestionElement.addEventListener("click", () => {
+                setOrigin(suggestion); // Update the state
+                originAutocomplete.innerHTML = ""; // Clear the autocomplete suggestions
+            });
+            originAutocomplete.appendChild(suggestionElement);
+        });
+
+        originAutocomplete.style.display = "block";
+    }
+
+    // Close the autocomplete dropdown when clicking outside of it
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            const originInput = document.getElementById("originInput");
+            const originAutocomplete = document.getElementById("originAutocomplete");
+
+            if (originInput && originAutocomplete &&
+                event.target !== originInput && event.target !== originAutocomplete) {
+                originAutocomplete.style.display = "none";
+            }
+        };
+
+        document.addEventListener("click", handleOutsideClick);
+
+        return () => {
+            document.removeEventListener("click", handleOutsideClick);
+        };
+    }, []);
 
     return (
         <div className="App">
@@ -115,18 +144,27 @@ function App() {
             <div className="button-container">
                 <button onClick={searchFlights}>Search Flights</button>
             </div>
+
+            <label htmlFor="originInput">Origin:</label>
             <input
                 type="text"
-                placeholder="Origin"
+                id="originInput"
+                placeholder="Enter origin"
                 value={origin}
-                onChange={(e) => setOrigin(e.target.value)}
+                onChange={(e) => handleOriginInput(e.target.value)}
             />
-            <input
-                type="text"
-                placeholder="Destination"
+            <div id="originAutocomplete" className="autocomplete"></div>
+
+            <select
                 value={destination}
                 onChange={(e) => setDestination(e.target.value)}
-            />
+            >
+                <option value="">Select Destination</option>
+                <option value="IAS">IAS</option>
+                <option value="MIL">MIL</option>
+                <option value="BUC">BUC</option>
+                {/* Add more options as needed */}
+            </select>
             <input
                 type="date"
                 value={departureDate}
